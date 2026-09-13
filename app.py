fe94204bc90ab1f14b4f54808baba95fe707f724
orzuqulovazizbek4-gif <orzuqulovazizbek4@gmail.com>
2026-09-13 18:51:04 +0500

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import streamlit as st
 <meta name="google-site-verification" content="0rGPUCPwegOEWNshxmATW_vNggxbO7GQlGC4x9W_9-0" />
+import streamlit as st
 import google.generativeai as genai
 from docx import Document
 import io
